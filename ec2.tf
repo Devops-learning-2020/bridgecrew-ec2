d8d6b43813505681459f7834b8415d30569d3fd6
vamshibeju <vamshi.beju@gmail.com>
2021-09-29 01:00:47 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -99,21 +99,21 @@ resource "aws_security_group" "web-node" {
     to_port   = 80
     protocol  = "tcp"
     cidr_blocks = [
-    "0.0.0.0/0"]
+    "10.0.0.1/32"]
   }
   ingress {
     from_port = 22
     to_port   = 22
     protocol  = "tcp"
     cidr_blocks = [
-    "0.0.0.0/0"]
+    "10.0.0.1/32"]
   }
   egress {
     from_port = 0
     to_port   = 0
     protocol  = "-1"
     cidr_blocks = [
-    "0.0.0.0/0"]
+    "10.0.0.1/32"]
   }
   depends_on = [aws_vpc.web_vpc]
   tags = {
