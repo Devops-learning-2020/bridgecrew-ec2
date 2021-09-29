e1aa5e45384d984d7d33f393de475ff402b7eb7b
vamshibeju <vamshi.beju@gmail.com>
2021-09-29 12:31:52 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -292,7 +292,56 @@ resource "aws_kms_key" "s3key" {
   description             = "KMS key 1"
   deletion_window_in_days = 10
 }
+resource "aws_s3_bucket" "flowbucket" {
+  bucket        = "${local.resource_prefix.value}-flowlogs"
+  force_destroy = true
+  versioning {
+    enabled = true
+  }
+  replication_configuration {
+   role = aws_iam_role.replication.arn
+    rules {
+    id     = "foobar"
+      prefix = "foo"
+       status = "Enabled"
 
+   destination {
+      bucket        = aws_s3_bucket.destination.arn
+      storage_class = "STANDARD"
+    }
+   }
+ }
+   server_side_encryption_configuration {
+  rule {
+     apply_server_side_encryption_by_default {
+       kms_master_key_id = aws_kms_key.s3key.arn
+       sse_algorithm     = "aws:kms"
+      }
+  }
+  }
+   dynamic "logging" {
+    for_each = var.logging
+    content {
+       target_bucket = logging.value["target_bucket"]
+       target_prefix = "log/${var.s3_bucket_name}"
+      }
+ }
+ 
+
+  tags = merge({
+    Name        = "${local.resource_prefix.value}-flowlogs"
+    Environment = local.resource_prefix.value
+    }, {
+    git_commit           = "d68d2897add9bc2203a5ed0632a5cdd8ff8cefb0"
+    git_file             = "terraform/aws/ec2.tf"
+    git_last_modified_at = "2020-06-16 14:46:24"
+    git_last_modified_by = "nimrodkor@gmail.com"
+    git_modifiers        = "nimrodkor"
+    git_org              = "bridgecrewio"
+    git_repo             = "terragoat"
+    yor_trace            = "f058838a-b1e0-4383-b965-7e06e987ffb1"
+  })
+}
 resource "aws_s3_bucket_public_access_block" "access_good_1" {
   bucket = aws_s3_bucket.flowbucket.id
 
