1e342c2b2c358bca22aa5c908e76ee8bf38d0eb6
vamshibeju <vamshi.beju@gmail.com>
2021-09-29 12:27:34 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -318,14 +318,7 @@ resource "aws_s3_bucket" "flowbucket" {
        target_prefix = "log/${var.s3_bucket_name}"
       }
  }
-  server_side_encryption_configuration {
-  rule {
-     apply_server_side_encryption_by_default {
-       kms_master_key_id = aws_kms_key.s3key.arn
-       sse_algorithm     = "aws:kms"
-      }
-  }
-  }
+ 
 }
   tags = merge({
     Name        = "${local.resource_prefix.value}-flowlogs"
